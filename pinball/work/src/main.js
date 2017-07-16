f73e0f1efe0698f6514b641baf5ed4a3000ea24a
rikuTanide <riku@tanide.net>
2017-07-16 11:46:12 +0900

    壁を築いた

--- pinball/work/src/main.js
+++ pinball/work/src/main.js
@@ -13,17 +13,22 @@ var render = Render.create({
     element: document.body,
     engine: engine,
     options: {
-        width: 800,
-        height: 600,
+        width: 300,
+        height: 530,
     }
 });
 
+
+var wall_left = Bodies.rectangle(0, 530 / 2, 10, 530, {isStatic: true});
+var wall_right = Bodies.rectangle(295, 530 / 2, 10, 530, {isStatic: true});
+var wall_bottom = Bodies.rectangle(150, 525, 300, 10, {isStatic: true});
+var wall_top = Bodies.rectangle(150, 5, 300, 10, {isStatic: true});
 
 var body = Bodies.circle(100, 100, 10, {
     friction: 0,
     restitution: 0.6,
 });
-World.add(world, body);
+World.add(world, [body, wall_left, wall_bottom, wall_right, wall_top]);
 
 Engine.run(engine);
 Render.run(render);
